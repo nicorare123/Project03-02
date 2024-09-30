a8b633553d38345b770c65e14c14e64a02f6d010
nicorare123 <nicorare123@gmail.com>
2024-09-30 21:29:10 +0900

    0930upload

---
+++ singleproject/Assets/Scripts/MainGimic.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGimic : MonoBehaviour
+{
+    public Image darkness;
+    private float lightFadeTime = 0.5f;
+    private GameObject[] traps;
+    private bool isLightOn = true;
+    private bool isFading = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        traps = GameObject.FindGameObjectsWithTag("Trap");
+        SetOverlayAlpha(0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleLight();
+        }
+    }
+
+    void ToggleLight()
+    {
+        isLightOn = !isLightOn;
+        StartCoroutine(FadeToDarkness(isLightOn ? 0f : 0.98f));
+    }
+
+    IEnumerator FadeToDarkness(float targetAlpha)
+    {
+        isFading = true;
+        float startAlpha = darkness.color.a;
+        float time = 0;
+
+        while (time < lightFadeTime)
+        {
+            time += Time.deltaTime;
+            float t = time / lightFadeTime;
+
+            SetOverlayAlpha(Mathf.Lerp(startAlpha, targetAlpha, t));
+
+            yield return null;
+        }
+
+        SetOverlayAlpha(targetAlpha);
+        isFading = false;
+    }
+
+    void SetOverlayAlpha(float alpha)
+    {
+        Color color = darkness.color;
+        color.a = alpha;
+        darkness.color = color;
+    }
+}
